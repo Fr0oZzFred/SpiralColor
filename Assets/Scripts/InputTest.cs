02ee95388d9ccd706cd1141a2190805988617792
Frédéric Deiber <frederic.deiber@gmail.com>
2022-01-31 11:48:32 +0100

    Update InputTest for Xbox Controller

--- Assets/Scripts/InputTest.cs
+++ Assets/Scripts/InputTest.cs
@@ -2,7 +2,8 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.InputSystem.DualShock;
-using UnityEngine.InputSystem.Switch;
+using UnityEngine.InputSystem.XInput;
+using UnityEngine.InputSystem.Controls;
 
 public class InputTest : MonoBehaviour
 {
@@ -10,13 +11,19 @@ public class InputTest : MonoBehaviour
     private Transform m_transform;
     public float sensibility;
     public Color pink;
-    SwitchProControllerHID cont;
+    XInputController xboxController;
     
     private void Start() {
+        
         try {
             this.controller = DS4.GetController();
         } catch {
-            Debug.LogWarning(Gamepad.current + " is not supported");
+            try {
+                xboxController = (XInputController)Gamepad.current;
+            } catch {
+                Debug.LogWarning(Gamepad.current.name + " is not supported");
+            }
+
         }
         m_transform = this.transform;
     }
@@ -35,7 +42,9 @@ public class InputTest : MonoBehaviour
             }
             if (controller.triangleButton.isPressed) {
                 GetComponent<MeshRenderer>().material.color = Color.green;
-                controller.SetLightBarColor(Color.green);
+                try {
+                    controller.SetLightBarColor(Color.green);
+                } catch { }
             }
 
             if (controller.circleButton.isPressed) {
@@ -52,10 +61,48 @@ public class InputTest : MonoBehaviour
                 GetComponent<MeshRenderer>().material.color = pink;
                 controller.SetLightBarColor(pink);
             }
+            m_transform.rotation *= DS4.GetRotation(sensibility * Time.deltaTime);
         }
-        m_transform.rotation *= DS4.GetRotation(sensibility * Time.deltaTime);
+
+
+
+        if(controller != null && xboxController == null) {
+            try {
+                xboxController = (XInputController)Gamepad.current;
+            } catch (Exception e) {
+                Console.WriteLine(e);
+            }
+        } else {
+
+            if (xboxController.startButton.isPressed) {
+                m_transform.rotation = Quaternion.identity;
+            }
+
+            if (xboxController.triangleButton.isPressed) {
+                GetComponent<MeshRenderer>().material.color = Color.green;
+            }
+
+            if (xboxController.circleButton.isPressed) {
+                GetComponent<MeshRenderer>().material.color = Color.red;
+            }
+
+            if (xboxController.buttonSouth.isPressed) {
+                GetComponent<MeshRenderer>().material.color = Color.blue;
+            }
+
+            if (xboxController.squareButton.isPressed) {
+                GetComponent<MeshRenderer>().material.color = pink;
+            }
+            Quaternion newRot = Quaternion.Euler(
+                m_transform.rotation.x* xboxController.leftStick.x.ReadValue() * sensibility,
+                m_transform.rotation.y* xboxController.leftStick.y.ReadValue() * sensibility,
+                m_transform.rotation.z
+                );
+            m_transform.rotation = newRot;
+        }
     }
     private void OnApplicationQuit() {
+        if(controller != null)
         controller.SetLightBarColor(Color.black);
     }
 }
