f278980205154543968cb2340f69dc68005d2861
Fr0oZzFred <78353908+Fr0oZzFred@users.noreply.github.com>
2022-02-19 20:27:04 +0100

    ColorController

--- Assets/Scripts/ColorController.cs
+++ Assets/Scripts/ColorController.cs
@@ -1,7 +1,19 @@
 ﻿using UnityEngine;
+using System.Collections.Generic;
 
 [System.Serializable]
-public class ColorController {
+public class ColorController : ISerializationCallbackReceiver {
     public Color color;
+    [ListToPopup(typeof(ColorController), "TMPList")]
     public string button;
+
+    public static List<string> TMPList;
+    [HideInInspector] public List<string> PopupList = new List<string> 
+    { "triangleButton", "circleButton", "crossButton", "squareButton"};
+
+    public void OnBeforeSerialize() {
+        TMPList = PopupList;
+    }
+
+    public void OnAfterDeserialize() { }
 }
