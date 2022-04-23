f36890751b10f944c044ad5d20ebccf1e0154bf5
Frédéric Deiber <frederic.deiber@gmail.com>
2022-04-23 17:02:26 +0200

    gems

--- Assets/Scripts/Environment/Gems.cs
+++ Assets/Scripts/Environment/Gems.cs
@@ -1,8 +1,14 @@
 using UnityEngine;
 public class Gems : MonoBehaviour {
-    //public Material transparent;
+
+    [SerializeField] float speed, amplitude;
+    [SerializeField] Vector3 rotSpeed;
+
+    Vector3 startPos;
+
     public int gemIndex;
     private void Start() {
+        startPos = transform.position;
         if (GameManager.Instance.gemsList[LevelManager.Instance.LevelInt].Count < FindObjectsOfType(typeof(Gems)).Length)
             GameManager.Instance.AddGem();
         else if (GameManager.Instance.CheckGem(gemIndex)) gameObject.SetActive(false);
@@ -14,5 +20,13 @@ public class Gems : MonoBehaviour {
     public void Collect() {
         if(!GameManager.Instance.gemsList[LevelManager.Instance.LevelInt][gemIndex]) GameManager.Instance.CollectGem(gemIndex);
         gameObject.SetActive(false);
+    }
+    private void Update() {
+        transform.Rotate(rotSpeed * Time.deltaTime);
+        transform.position = new Vector3(
+            startPos.x,
+            startPos.y + Mathf.Sin(Time.time * speed) * amplitude,
+            startPos.z
+        );
     }
 }
